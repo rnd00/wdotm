7198d29b34523d25f70542fa63c5470ec8433ff7
rnd00 <rdiedan@gmail.com>
2022-07-15 14:14:01 +0900

    update: move to different files

---
+++ days.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+const (
+	MON Days = iota
+	TUE
+	WED
+	THU
+	FRI
+	SAT
+	SUN
+)
+
+type Days int
+
+func (d Days) String() string {
+	W := []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+	return W[d]
+}
+
+func (d Days) Less(i, j Days) bool {
+	return i < j
+}
+
+func calculate(day Days, totalDay int) map[Days]int {
+	ret := make(map[Days]int)
+	for i := 0; i < totalDay; i++ {
+		ret[(Days(i)+day)%7]++
+	}
+	return ret
+}
+
+func printWeekdaysOnly(data map[Days]int) {
+	for i := MON; i < SAT; i++ {
+		fmt.Printf("%10s:%3d\n", i, data[i])
+	}
+}
+
+func printDays(data map[Days]int, start, end Days) {
+	for i := start; i < end; i++ {
+		fmt.Printf("%10s:%3d\n", i, data[i])
+	}
+}
--- main.go
+++ main.go
@@ -2,38 +2,7 @@ package main
 
 import "fmt"
 
-const (
-	MON Days = iota
-	TUE
-	WED
-	THU
-	FRI
-	SAT
-	SUN
-)
-
-type Days int
-
-func (d Days) String() string {
-	W := []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
-	return W[d]
-}
-
 func main() {
 	fmt.Println("wdotm")
 	printWeekdaysOnly(calculate(FRI, 31))
-}
-
-func calculate(day Days, totalDay int) map[Days]int {
-	ret := make(map[Days]int)
-	for i := 0; i < totalDay; i++ {
-		ret[(Days(i)+day)%7]++
-	}
-	return ret
-}
-
-func printWeekdaysOnly(data map[Days]int) {
-	for i := MON; i < SAT; i++ {
-		fmt.Printf("%10s:%3d\n", i, data[i])
-	}
 }
